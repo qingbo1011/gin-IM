14fc3b16082bdee4c64084e7974516cbf5401038
qingbo1011 <61835535+qingbo1011@users.noreply.github.com>
2022-07-15 15:20:02 +0800

    model user

--- route/route.go
+++ route/route.go
@@ -7,6 +7,10 @@ import (
 func NewRoute() *gin.Engine {
 	// gin.Default()和gin.New()的区别：gin.Default()默认使用了2个中间件Logger(), Recovery()
 	r := gin.Default()
-
+	//user := r.Group("/api/user")
+	//{
+	//	user.POST("/register", api.UserRegister)
+	//	user.POST("/login", api.Login)
+	//}
 	return r
 }
